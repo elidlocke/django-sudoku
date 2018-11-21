76eeb29617f8ab91d6f95773d7ee8f864b75155d
Elizabeth Locke <elidlocke@gmail.com>
2018-11-21 15:38:55 -0500

    change the admin display

--- game_solver/admin.py
+++ game_solver/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Game
 
-admin.site.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    fields = ['input', 'output', 'creation_date']
+    list_display = ('creation_date', 'input', 'output')
+
+admin.site.register(Game, GameAdmin)
